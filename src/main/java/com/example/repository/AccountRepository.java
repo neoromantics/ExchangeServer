af4e8c4f104eebc440d9911c1daa0cb0e04e71b2
TairanD <tairan.ding@duke.edu>
2025-03-26 23:09:29 -0400

    no orm

--- src/main/java/com/example/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
